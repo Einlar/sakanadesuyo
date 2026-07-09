a683f577c495efda13cbbe7ad0a137e9f450557a
Francesco Manzali <manzali.francesco97@gmail.com>
2026-07-10 01:01:28 +0200

    change default model

--- src/lib/constants.ts
+++ src/lib/constants.ts
@@ -1,5 +1,5 @@
 /**
  * Default LLMs
  */
-export const DEFAULT_ANALYSIS_MODEL = 'openai/gpt-4.1-mini';
+export const DEFAULT_ANALYSIS_MODEL = 'openai/gpt-5.6-luna';
 export const OCR_MODEL = 'openai/gpt-4o';
